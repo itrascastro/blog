5b528e9a236398b3c92f66790bf0039daa346b17
itrascastro <itrascastro@gmail.com>
2023-02-19 22:53:44 +0100

    Thymeleaf layout added and working

--- src/main/java/org/itrascastro/blog/controller/IndexController.java
+++ src/main/java/org/itrascastro/blog/controller/IndexController.java
@@ -2,7 +2,6 @@ package org.itrascastro.blog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 
 @Controller
 public class IndexController {
